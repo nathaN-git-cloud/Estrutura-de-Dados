52fddbbb977532753d085ed9727f6190d6c279ac
EZM <nathanmp201@gmail.com>
2026-04-14 12:10:55 -0300

    Ordena.h

---
+++ Ordena.h
@@ -0,0 +1,12 @@
+#include <iostream>
+class Ordena
+{
+    public:
+        void geraVetor(int[], int);
+        void copiarVetor(int[], int[], int);
+        void exibirVetor(int[], int);
+        void Bolha(int[], int, int*, int*);
+        void BolhaMelhorado(int[], int, int*, int*);
+        void Insercao(int[], int, int*, int*);
+        void Selecao(int[], int, int*, int*);
+};
